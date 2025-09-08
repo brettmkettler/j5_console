97f4ae2fac26b5ace8d9ddc0e44069b08e0420fb
Brett Kettler <68669493+brettmkettler@users.noreply.github.com>
2025-09-07 23:56:02 -0400

    ddd

--- j5_console.py
+++ j5_console.py
@@ -144,7 +144,7 @@ servo_config = {
         'center_angle': 75,  # Center/neutral position
         'description': 'Left door servo',
         'open_angle': 50,    # Angle for open position
-        'closed_angle': 110  # Angle for closed position
+        'closed_angle': 90  # Angle for closed position
     },
     'console_door': {
         'device': console_door,
